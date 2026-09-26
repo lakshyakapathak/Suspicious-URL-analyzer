62668f250515a5ded1d896b73234c82f48d33fd2
Lakshyaka Pathak <lakshyakapathak@gmail.com>
2026-09-26 19:52:30 +0530

    completed the risk analyzer

--- src/rules.py
+++ src/rules.py
@@ -37,3 +37,49 @@ if __name__ == "__main__":
         for rule in rules:
             print(f"  {rule.__name__}: {rule(features)}")
         print()
+    #test cases to decide on risk levels
+    TESTCASES = [
+    # Category 1: Clearly legitimate, short
+    "https://www.google.com",
+    "https://github.com/torvalds/linux",
+    "https://music.youtube.com/watch?v=CvlWnG1yiyQ",
+    "https://raptor.martincarlisle.com/",
+
+    # Category 2: Clearly legitimate, long/complex
+    "https://www.simonandschuster.com/books/21-Days-of-Fear/Herve-Commere/9798347119615",
+    "https://www.amazon.com/s?k=laptop+stand&ref=nb_sb_noss_2",
+    "https://www.myntra.com/sweaters/h%26m/hm-women-ribbed-pullover/45333756/buy",
+
+    # Category 3: Legitimate but with a superficial red flag
+    "https://accounts.google.com/signin/v2/identifier",
+    "https://www.paypal.com/signin",
+    "https://yonobusiness.sbi.bank.in/yonobusinesslogin",
+
+    # Category 4: Classic phishing pattern — IP-based 
+    "http://192.168.1.1/login",
+    "http://45.33.32.156/verify-account",
+    "http://103.45.22.9/sbi-verify-account",
+
+    # Category 5: Classic phishing pattern — @ symbol trick 
+    "http://accounts-google.com@evil-site.com/",
+    "http://paypal.com@192.168.5.5/secure",
+
+    #Category 6: Classic phishing pattern — keyword + hyphen stuffing
+    "https://secure-login-verify-account.example.com/update",
+    "http://paypal-secure-update-account.info/verify",
+    "http://yono-sbi-secure-login.xyz/update-kyc",
+
+    # Category 7: Ambiguous / borderline
+    "https://mybank-secure-portal.com/dashboard",
+    "http://user@fake-bank.com/verify",
+    "http://sbi.bank.in.verify-account-now.com/login",
+    "http://127.0.0.1:52400/signin?nonce=En2c5OlTxCLzJ7fdo30hAQ==",
+]
+    for p in TESTCASES:
+        features = feature_extractor(p)
+        print(p)
+        for rule in rules:
+            print(f"  {rule.__name__}: {rule(features)}")
+        print()
+
+
